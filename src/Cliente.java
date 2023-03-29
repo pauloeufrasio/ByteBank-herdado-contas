90d47b4ecb041117744a17f81ce2eda96e3f6794
pauloeufrasio <henriquepaulo0606@gmail.com>
2023-03-29 15:53:12 -0300

    Atividade herança e construtores

---
+++ src/Cliente.java
@@ -0,0 +1,40 @@
+public class Cliente {
+    // Criando get quando o valor so retorna
+    public String getNome() {
+        return nome;
+    }
+    // Criando set quando for edita alguma informação
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getProfissão() {
+        return profissão;
+    }
+
+    public void setProfissão(String profissão) {
+        this.profissão = profissão;
+    }
+
+    public Cliente getTitular() {
+        return titular;
+    }
+
+    public void setTitular(Cliente titular) {
+        this.titular = titular;
+    }
+
+    private String nome;
+    private String cpf;
+    private String profissão;
+    // Criando conta do tipo classe
+    Cliente titular;
+}
---
+++ src/ContaCorrente.java
@@ -0,0 +1,7 @@
+public class ContaCorrente extends Conta  {
+    public ContaCorrente(int agencia, int numero){  // Criando construtor especifico dessa classe atribuirdo parametro agencia e numero
+        super(agencia,numero); // chamando classe mae
+
+
+    }
+}
