90b5465e17b1cd11869490f04035b3a8b5760e14
pauloeufrasio <henriquepaulo0606@gmail.com>
2023-04-09 21:47:41 -0300

    Utilizando classe object

---
+++ src/br/com/banco/bytebank/modelo/teste/Teste.java
@@ -0,0 +1,29 @@
+package br.com.banco.bytebank.modelo.teste;
+
+import br.com.banco.bytebank.modelo.Conta;
+import br.com.banco.bytebank.modelo.ContaCorrente;
+import br.com.banco.bytebank.modelo.ContaPoupanca;
+
+public class Teste {
+    public static void main(String[] args) {
+        System.out.println("x");
+        System.out.println(3);
+        System.out.println(false);
+        ContaCorrente cc = new ContaCorrente(22, 33);
+        ContaPoupanca cp = new ContaPoupanca(33, 22);
+        System.out.println(cc);
+        System.out.println(cp);
+
+        println(cc);
+    }
+
+    static void println() {}
+
+    static void println(int a) {}
+
+    static void println(boolean valor) {}
+
+    static void println(Conta conta){}
+
+}
+
