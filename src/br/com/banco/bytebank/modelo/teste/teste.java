cd1def5e3e8eda5b44a01e44888ebeca3b7e23e4
paulo <henriquepaulo0606@gmail.com>
2023-04-17 12:57:57 -0300

    teste de Array com laço for

--- src/br/com/banco/bytebank/modelo/teste/teste.java
+++ src/br/com/banco/bytebank/modelo/teste/teste.java
@@ -6,21 +6,29 @@ public class teste {
     public static void main(String[] args) {
 
         // Cria Array e atribuir 5 idades
-        int idades [] = new int[5];// inicializa o array com os valores padroes
+        int idades[] = new int[5];// inicializa o array com os valores padroes
 
         // inicializar array colocar na posição desejada e atribuir um valor pra cada posição
-        idades[0] = 29;
-        idades[1] = 39;
-        idades[2] = 49;
-        idades[3] = 59;
-        idades[4] = 69;
+        //idades[0] = 29;
+        //idades[1] = 39;
+        //idades[2] = 49;
+        //idades[3] = 59;
+        //idades[4] = 69;
 
         // acessar posição atraves dos numeros da idade
-        int idade1 = idades[4];
+        //int idade1 = idades[4];
 
-        System.out.println(idade1);
+        //System.out.println(idade1);
 
+        // Iniciaizar array com laço for
+        for (int i = 0; i < idades.length; i++) {
+            idades[i] = i * i;
+        }
+        // imprimir array com laço for
+        for (int i = 0; i < idades.length; i++) {
+            System.out.println(idades[i]);
 
+        }
 
     }
 }
