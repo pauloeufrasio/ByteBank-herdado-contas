fae7582366a9b3e23dae745c55ca39e4c4139a1d
paulo <henriquepaulo0606@gmail.com>
2023-05-11 14:28:15 -0300

    Aprendendo sobre Autoboxing e unboxing e Integer

--- src/br/com/banco/bytebank/modelo/teste/util/Teste.java
+++ src/br/com/banco/bytebank/modelo/teste/util/Teste.java
@@ -6,17 +6,24 @@ import java.util.List;
 public class Teste {
     public static void main(String[] args) {
 
-        int[] idades = new int[5];
-        String[] nomes = new String[5];
+        int idade = 29;
+        Integer idadeRef = Integer.valueOf(29); //autoboxing
+        System.out.println(idadeRef.doubleValue());
 
-        // idade é um tipo primitivo
-        int idade = 29; // Integer
-        // int é um primitivo, não uma referência. Assim, o certo é utilizarmos o Integer
-        Integer idadeRef = new Integer(29);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+
+        System.out.println(Integer.SIZE);
+        System.out.println(Integer.BYTES);
+
+        int valor = idadeRef.intValue(); //unboxing
+        String s = args[0];//"10"
+        //Integer numero = Integer.valueOf(s);
+        int numero = Integer.parseInt(s);
+        System.out.println(numero);
+
         List<Integer> numeros = new ArrayList<Integer>();
-        numeros.add(idade);
-
-
+        numeros.add(29); //Autoboxing
 
     }
 }
